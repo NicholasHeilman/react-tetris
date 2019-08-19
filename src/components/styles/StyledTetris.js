cbbacc187fe7eb4d9910399143528a861be57e73
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-08-19 17:35:56 -0500

    add grid from styled components

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -7,7 +7,7 @@ export const StyledWrapper = styled.div`
     background: url(${bgImage}) #000;
     background-size: cover;
     overflow: hidden;
-`
+`;
 
 export const StyledTetris = styled.div`
     display: flex;
