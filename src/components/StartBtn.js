f95bc0f9a1d8e221e1336547c05fb5238d6674ee
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-08-21 14:40:58 -0500

    add keyCode move functions

--- src/components/StartBtn.js
+++ src/components/StartBtn.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyledStartBtn } from './styles/StyledStartBtn';
 
-function startBtnClick (){
-    console.log('StartBtn Click')
-}
+
 
 const StartBtn =({ callback }) => (
-    <StyledStartBtn onClick={startBtnClick}>
+    <StyledStartBtn >
         Start Game
     </StyledStartBtn>
 )
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { createStage } from '../gameHelpers';
 
 //Components
 import Stage from "./Stage";
@@ -20,8 +21,39 @@ const Tetris = () => {
 
 
     console.log('re-render');
+
+    const movePlayer = dir => {
+        updatePlayerPos({ x: dir, y: 0 })
+    }
+
+    const startGame = () => {
+        //reset game
+        setStage(createStage());
+        resetPlayer();
+    }
+
+    const drop = () => {
+        updatePlayerPos({ x: 0, y: 1, collided: false});
+    }
+
+    const dropPlayer = () => {
+        drop();
+    }
+
+    const move = ({ keyCode }) => {
+        if(!gameOver){
+            if(keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39){
+                movePlayer(1);
+            } else if (keyCode === 40){
+                dropPlayer();
+            }
+        }
+    }
+
     return(
-        <StyledWrapper>
+        <StyledWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
             <Stage stage={stage} />
             <aside>
@@ -34,7 +66,7 @@ const Tetris = () => {
                     <Display text="Level" />
                 </div>
                 )};
-                <StartBtn />
+                <StartBtn onClick={startGame}/>
             </aside>
             </StyledTetris>
         </StyledWrapper>
