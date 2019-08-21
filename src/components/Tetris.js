611a8594c5c6643f2a8a2d306ff15078125d1806
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-08-21 14:00:55 -0500

    add gameOver ternary operator

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+//Components
 import Stage from "./Stage";
 import Display from './Display';
 import StartBtn from './StartBtn';
-import { createStage } from '../gameHelpers';
+
+//Styled Components
 import { StyledWrapper, StyledTetris } from './styles/StyledTetris';
 
+//Custom Hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+
 const Tetris = () => {
-    // console.log(createStage());
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+
+    console.log('re-render');
     return(
         <StyledWrapper>
             <StyledTetris>
-            <Stage stage={createStage()} />
+            <Stage stage={stage} />
             <aside>
+                {gameOver ? (
+                    <Display gameOver={gameOver} text="Game Over" />
+                ) : (
                 <div>
                     <Display text="Score" />
                     <Display text="Row" />
                     <Display text="Level" />
                 </div>
+                )};
                 <StartBtn />
             </aside>
             </StyledTetris>
