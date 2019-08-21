dfb8dd28d3a865d4aa701584aea32d0d16a68ac5
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-08-21 15:11:56 -0500

    add usePayer custom hook

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,7 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
-export const useStage = () => {
+export const useStage = (player, setPlayer) => {
     const [stage, setStage] = useState(createStage());
+    
+    useEffect(() => {
+        const updateStage = prevStage => {
+            const newStage = prevStage.map(row =>
+                row.map(cell => ())
+                )
+        }
+        setStage(prev => updateStage(prev))
+    }, [])
+
     return [stage, setStage];
 }
