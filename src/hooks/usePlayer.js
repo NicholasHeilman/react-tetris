904d4627b7ce83944f15835f5f82535a6113258f
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-09-06 11:13:41 -0500

    add rotate function

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -9,6 +9,22 @@ export const usePlayer = () => {
         collided: false,
     });
 
+    const rotate = (matrix, dir) => {
+        //make row into columns
+        const rotatedTetro = matrix.map((_, index) =>
+            matrix.map(col => col[index]),
+        );
+        //reverse each row to rotate
+        if(dir > 0) return rotatedTetro.map(row => row.reverse());
+        return rotatedTetro.reverse();
+    }
+
+    const playerRotate = (stage, dir) => {
+        const clonedPlayer = JSON.parse(JSON.stringify(player));
+        clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+        setPlayer(clonedPlayer);
+    }
+
     const updatePlayerPos =({ x, y, collided }) => {
         setPlayer(prev => ({
             ...prev,
@@ -25,5 +41,5 @@ export const usePlayer = () => {
         })
     })
 
-    return [player, updatePlayerPos, resetPlayer]; 
+    return [player, updatePlayerPos, resetPlayer, playerRotate]; 
 }
